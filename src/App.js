39eaeae5886fd527a36fd2c96ae7d59a52a81ca6
Kir14 <62447478+Kir14@users.noreply.github.com>
2020-07-16 00:15:22 +0300

    front

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Members from "./Pages/Members";
+import {Navbar} from "react-bootstrap";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <NavDropdown title="Action" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Hands up</NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Hand down</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+                <Nav className="ml-auto">
+                    <NavDropdown title="Account" id="basic-nav-dropdown">
+                        <NavDropdown.Item href="#action/3.1">Login </NavDropdown.Item>
+                        <NavDropdown.Item href="#action/3.2">Logout</NavDropdown.Item>
+                    </NavDropdown>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+      <Members />
     </div>
   );
 }
